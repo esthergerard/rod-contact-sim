2d6342ba290fb994d0b6c2576bbb722bacc1548b
esthergerard <esther-gerard@orange.fr>
2024-03-26 13:55:43 -0700

    Augmentation progressive rayon

--- augmentRadius.py
+++ augmentRadius.py
@@ -27,39 +27,48 @@ filepath = 'option.txt'
 parsed_data = parse_file(filepath)
 print(parsed_data)
 
-# Change "rodRadius" to add it 0.000025 while it is less than 0.006
-while parsed_data['rodRadius'] < 0.006:
-    parsed_data['rodRadius'] = parsed_data['rodRadius'] + 0.000025
+# Change "rodRadius" to add it 0.00002 while it is less than 0.0051
+while parsed_data['rodRadius'] < 0.0045:
+    parsed_data['rodRadius'] = parsed_data['rodRadius'] + 0.00002
     print(parsed_data)
-    
+    # Write the updated data to a new file
+    with open('option.txt', 'w') as file:
+        for key, value in parsed_data.items():
+            # Check if the value is a tuple or list
+            if isinstance(value, (tuple, list)):
+                # Convert all values to strings and join them with a space
+                value_str = ' '.join(str(v) for v in value)
+            else:
+                value_str = str(value)
+            file.write(f'{key} {value_str}\n')
+
     # Run the simulation with the new rodRadius
     # Specify the path to your text file
     file_path = 'Commands.txt'
 
+    print('Running the commands from the file:', file_path)
     # Read the content of the file
     with open(file_path, 'r') as file:
         # Read each line from the file
         for line in file:
             # Remove leading and trailing whitespaces
             command = line.strip()
-
             # Execute the command using subprocess
             subprocess.run(['bash', '-c', command])
-    
+
     # Copy the lines 16951 to 17289 from the file "configData" and use them to update the entire file tied-R1
-    with open('configData_numFlagella_1_EA_135400_dt_0.001_totalTime_0.511_imc_1_friction_0.txt', 'r') as file:
+    with open('datafiles/configData_numFlagella_1_EA_135400_dt_0.001_totalTime_0.511_imc_1_friction_0.txt', 'r') as file:
         configData = file.readlines()
-    with open('tied-R1', 'r') as file:
+    with open('knot_configurations/tied-R1', 'r') as file:
         tiedR1 = file.readlines()
-    with open('tied-R1', 'w') as file:
+    with open('knot_configurations/tied-R1', 'w') as file:
         for i, line in enumerate(tiedR1):
-            if 16951 <= i <= 17289:
-                file.write(configData[i])
-                # remove the first "0 " from the first line and the last line
-                if i == 16951 or i == 17289:
-                    file.write(line[2:])
-            else:
-                file.write(line)
-
-
-
+            index_in_file = i + 16950
+            if 1 <= index_in_file <= len(configData):
+            # remove the first "0 " from the first line and the last line of tiedR1
+                if i == 0:
+                    file.write(configData[index_in_file][2:])
+                elif i == len(tiedR1) - 1:
+                    file.write(configData[index_in_file][2:])
+                else:
+                    file.write(configData[index_in_file])
